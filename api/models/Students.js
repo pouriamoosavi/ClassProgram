df0b4ab576cc2fdc2df660bfe699638b3324b88f
Pouria Moosavi <pouria.moosvi@gmail.com>
2018-08-27 20:51:27 +0430

    Update Student.js
    Add mongoDB
    Remove present

--- api/models/Students.js
+++ api/models/Students.js
@@ -4,20 +4,12 @@
  * @description :: TODO: You might write a short summary of how this model works and what it represents here.
  * @docs        :: http://sailsjs.org/documentation/concepts/models-and-orm/models
  */
-
 module.exports = {
 
   attributes: {
-    firstName:{
-      type:'string'
-    },
-    lastName:{
-      type:'string'
-    },
-    present:{
-      type:'boolean'
-    }
+    firstName: {type:'string', required: true},
+    lastName: {type:'string'},
+    tel: {type:'string'}
   },
-  connection: 'Mongodb'
+  datastores: 'sailsDisk'
 };
-
